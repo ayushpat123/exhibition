7391dd62031b333870085604b62a52599a8365ea
ayushpat123 <ayushpat123@gmail.com>
2021-09-21 11:25:23 +0000

    Deploying to gh-pages from @ ayushpat123/exhibition@a481b71bd7cf1e2264ae8de058ea831f8aa922dd 🚀

---
+++ static/js/2.3e37ac51.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpexhibition=this.webpackJsonpexhibition||[]).push([[2],{16:function(n,r,a){"use strict";a.r(r);a(0);var o=a(34),t=a.n(o),i=a(1);r.default=function(n){var r=n.x,a=n.y;return Object(i.jsx)(t.a,{setup:function(n,o){n.createCanvas(r,a).parent(o),n.background(0),n.noStroke()},draw:function(n){for(var o=n.random(3),t=0;t<=o;t++)n.fill(n.random(256),n.random(100)),n.circle(n.random(r),n.random(a),n.random(15))}})}}}]);
+//# sourceMappingURL=2.3e37ac51.chunk.js.map
